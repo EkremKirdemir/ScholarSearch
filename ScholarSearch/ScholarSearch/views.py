929e8d8d6aa47efab0551ad527df2834b36a7f3d
EkremKirdemir <kirdemir.ekrem@gmail.com>
2024-03-12 01:58:40 +0300

    filtering and date sorting added to home page

--- ScholarSearch/ScholarSearch/views.py
+++ ScholarSearch/ScholarSearch/views.py
@@ -8,7 +8,9 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from bson import ObjectId
 from django.shortcuts import redirect
+import pymongo
 from .search import searchCorrection, search_and_parse
+import re
 
 @require_http_methods(["GET"])
 def search(request):
@@ -81,24 +83,78 @@ def paper_detail(request, object_id):
     return render(request, 'paper_detail.html', {'document': document})
 @require_http_methods(["GET"])
 def home(request):
+    # Database setup
     client = MongoClient('mongodb+srv://admin:admin@search0.sof6xtt.mongodb.net/')
     db = client['SearchScholar']
     collection = db['Results']
-    
-    order_by = request.GET.get('order_by', 'default')
 
-    if order_by == 'alphabetical':
-        documents = list(collection.find({}, {'_id': 1, 'paper_name': 1}).sort('paper_name', 1))
-    else:
-        documents = list(collection.find({}, {'_id': 1, 'paper_name': 1}))
+    # Retrieve filters and order_by value from the request
+    order_by = request.GET.get('order_by', 'default')
+    query = request.GET.get('query', '')
+    paper_name = request.GET.get('paper_name', '')
+    paper_authors = request.GET.get('paper_authors', '')
+    paper_abstract = request.GET.get('paper_abstract', '')
+    paper_publisher = request.GET.get('paper_publisher', '')
+    keywords = request.GET.get('keywords', '')
+    publisher_type = request.GET.get('publisher_type', '')
+
+    # Build query filters based on user input
+    query_filters = {}
+    if query:
+        query_filters['keywords_se'] = {"$regex": query, "$options": "i"}
+    if paper_name:
+        query_filters['paper_name'] = {"$regex": paper_name, "$options": "i"}
+    if paper_authors:
+        query_filters['paper_authors'] = {"$regex": paper_authors, "$options": "i"}
+    if paper_abstract:
+        query_filters['paper_abstract'] = {"$regex": paper_abstract, "$options": "i"}
+    if paper_publisher:
+        query_filters['paper_publisher_name'] = {"$regex": paper_publisher, "$options": "i"}
+    if keywords:
+        query_filters['keywords_paper'] = {"$regex": keywords, "$options": "i"}
+    if publisher_type:
+        query_filters['publisher_type'] = publisher_type
+
+    # Apply sorting if 'alphabetical' is selected
+    documents = list(collection.find(query_filters))
+
+    # Custom Python sorting if 'date' is selected
+    if order_by == 'date':
+        def extract_year(doc):
+            year_match = re.search(r'\b(19|20)\d{2}\b', doc.get('paper_date', ''))
+            if year_match:
+                return int(year_match.group(0))
+            return 9999 if doc.get('paper_date', '') == 'unknown' else 10000  # 'unknown' sorts before other non-year values
+
+        documents.sort(key=extract_year)
+
+    elif order_by == 'alphabetical':
+        documents.sort(key=lambda x: x.get('paper_name', '').lower())
+
+    # Assign string IDs for use in templates
     for doc in documents:
         doc['id_str'] = str(doc['_id'])
-    # Paginate the results
+
+    # Pagination
     paginator = Paginator(documents, 10)  # Show 10 documents per page
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    # Render the homepage with the search results
-    return render(request, 'home.html', {'page_obj': page_obj, 'order_by': order_by})
+    # Render the page with filtered and sorted results
+    return render(request, 'home.html', {
+        'page_obj': page_obj,
+        'order_by': order_by,
+        # Include the current filters in the context to repopulate the form
+        'current_filters': {
+            'query': query,
+            'paper_name': paper_name,
+            'paper_authors': paper_authors,
+            'paper_abstract': paper_abstract,
+            'paper_publisher': paper_publisher,
+            'keywords': keywords,
+            'publisher_type': publisher_type,
+        },
+        'request': request  # Pass the request to maintain form state
+    })
 
 
